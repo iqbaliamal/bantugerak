ef10e79e0b50ce9a4d3a3cb0bda210091ced7a40
iqbaliamal <iqbaliamal@gmail.com>
2021-06-09 20:52:45 +0700

    templating admin blade

---
+++ bantugerak-web/resources/views/admin/pages/home.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.layout.master')
+
+@section('title', 'Admin - Dashboard')
+@section('content')
+<!-- Main Content -->
+<div class="main-content">
+    <section class="section">
+
+    </section>
+</div>
+@endsection
+
+@push('cutomcss')
+
+@endpush
+
+@push('customjs')
+
+@endpush

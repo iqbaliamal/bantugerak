24d1ff03e7e67b7c422d7759fdc25cf53c7efa1b
iqbaliamal <iqbaliamal@gmail.com>
2021-06-10 08:38:24 +0700

    add auth dan session

---
+++ bantugerak-web/database/migrations/2021_06_10_012859_create_kegiatans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKegiatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kegiatans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kegiatan', 255);
+            $table->string('foto_kegiatan', 255);
+            $table->longText('deskripsi_kegiatan')->nullable();
+            $table->date('tanggal_kegiatan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kegiatans');
+    }
+}
